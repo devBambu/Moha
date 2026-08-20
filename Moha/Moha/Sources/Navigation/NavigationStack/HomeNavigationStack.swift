9f6d910d19056101d7b495e51a4f8219b96bd4cc
Bambu <byrr113@gmail.com>
2026-08-21 00:28:41 +0900

    ✨feat: HomeNavigationStack 생성 및 기초 구현

---
+++ Moha/Moha/Sources/Navigation/NavigationStack/HomeNavigationStack.swift
@@ -0,0 +1,28 @@
+//
+//  HomeNavigationStack.swift
+//  Moha
+//
+//  Created by 변예린 on 8/21/26.
+//
+
+import SwiftUI
+
+struct HomeNavigationStack: View {
+    @Bindable var router: HomeRouter
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            Text("Home") //TODO: HomeView로 수정 필요
+                .navigationDestination(for: HomeRoute.self) { route in
+                    destinationView(for: route)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private func destinationView(for route: HomeRoute) -> some View {
+        switch route {
+        case .createTodo:
+        }
+    }
+}

0b025db4a70166cf45cf495482827a2371416fe0
Bambu <byrr113@gmail.com>
2026-08-20 18:26:23 +0900

    ✨feat: MainRouter 생성 및 기초 구현

---
+++ Moha/Moha/Sources/Navigation/MainRouter.swift
@@ -0,0 +1,36 @@
+//
+//  MainRouter.swift
+//  Moha
+//
+//  Created by 변예린 on 8/20/26.
+//
+
+import SwiftUI
+import Combine
+
+final class MainRouter: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(to route: any Route) {
+        path.append(route)
+    }
+    
+    func pop() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+    
+    func reset() {
+        path = NavigationPath()
+    }
+    
+    func popTo(count: Int) {
+        guard count > 0, count <= path.count else { return }
+        path.removeLast(count)
+    }
+    
+    func replace(with route: any Route) {
+        path = NavigationPath()
+        path.append(route)
+    }
+}
